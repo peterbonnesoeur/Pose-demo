b3c78e65ba30977a8abaaf94772d4fed66008919
peterbonnesoeur <maxime.bonn@gmail.com>
2021-11-20 19:32:11 +0100

    Added basic pose estimation

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ import gradio as gr
 import openpifpaf
 import numpy as np
 
-
-predictor_animal = openpifpaf.Predictor(checkpoint='shufflenetv2k30-animalpose')
-predictor_whole_body = openpifpaf.Predictor(checkpoint='shufflenetv2k16-wholebody')
+predictor_pose = openpifpaf.Predictor(checkpoint='shufflenetv2k30')
+predictor_whole_body = openpifpaf.Predictor(checkpoint='shufflenetv2k30-wholebody')
 predictor_vehicle = openpifpaf.Predictor(checkpoint='shufflenetv2k16-apollo-24')
 
 
 def inference(img, ver):
-    
-    if ver == 'whole-body':
+    if ver == 'pose':
+        predictor = predictor_pose
+    elif ver == 'whole-body':
         predictor = predictor_whole_body
     elif ver == 'vehicles':
         predictor = predictor_vehicle
-    elif ver == 'animal':
-        predictor = predictor_animal
     else:
         raise ValueError('invalid version')
 
@@ -34,6 +32,18 @@ title = "Openpifpaf"
 description = "Gradio demo for openpifpaf. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below. Please use a cropped portrait picture for best results similar to the examples below"
 article = "<p style='text-align: center'><a href='https://github.com/openpifpaf/openpifpaf' target='_blank'>Github Repo Openpifpaf</a> | <a href='https://github.com/peterbonnesoeur' target='_blank'>Github Repo peterbonnesoeur</a></p>"
 
-examples=[ ['bill.png', 'whole-body'], ['vehicles.jpg', 'vehicles'], ['apolloscape.jpeg', 'vehicles'], ['dalmatian.jpg', 'animal'], ['elon.png','whole-body'], ['billie.png','whole-body']]
-gr.Interface(inference, [gr.inputs.Image(type="pil"),gr.inputs.Radio(['pose','whole-body', 'vehicles', 'animal'], type="value", default='whole-body', label='version')
+with open("article.html", "r", encoding='utf-8') as f:
+    article= f.read()
+
+examples=[ 
+    ['basketball.jpg','whole-body'],
+    ['meeting.jpeg','whole-body'],
+    ['crowd.jpg','pose'],
+    ['elon.png','whole-body'],
+    ['billie.png','whole-body'],
+    ['india-vehicles.jpeg', 'vehicles'],
+    ['russia-vehicles.jpg', 'vehicles'],
+    ['paris-vehicles.jpg', 'vehicles'],
+    ]
+gr.Interface(inference, [gr.inputs.Image(type="pil"),gr.inputs.Radio(['pose', 'whole-body', 'vehicles'], type="value", default='pose', label='version')
 ], gr.outputs.Image(type="pil"),title=title,description=description,article=article,enable_queue=True,examples=examples).launch()
